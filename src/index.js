015de3f1032a63aeaf505084374fc2f2d62975ab
Dane Smith <dane.smith@optum.com>
2017-11-29 21:04:36 -0600

    remove comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function message() {
     var message = "";
-  // Lodash, currently included via a script, is required for this line to work
   message = _.join(['Hello', 'webpack', ''], ' ');
 
   return message;
@@ -10,7 +9,6 @@ function component() {
   var element = document.createElement('div');
   element.className = "my-cool-style"
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'Canopus'], ' ');
 
   return element;
