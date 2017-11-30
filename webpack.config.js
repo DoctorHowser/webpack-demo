3d190afd517e05f23b3c3bc37e5cad24a389170f
Dane Smith <dane.smith@optum.com>
2017-11-29 21:06:20 -0600

    comment out

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// const path = require('path');
+
+// module.exports = {
+//   entry: './app.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader'
+//         ]
+//       },
+//  // todo add ttf
+//     ]
+//   }
+  // Plugins could go here!
+};
